101f3bc464f9b9f7820d8046f5b5459a3dfd7983
duc <trinhminhduc.0500@gmail.com>
2018-10-09 23:51:37 +0700

    if_conditional

--- src/duc/dev/Basic.java
+++ src/duc/dev/Basic.java
@@ -1,34 +1,45 @@
 package duc.dev;
  
-import java.text.DecimalFormat;
 import java.util.Scanner;
  
-public class Basic{
- 
+public class Basic {
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        DecimalFormat decimalFormat = new DecimalFormat("#.##");
-         
-        System.out.println("Nhap vao so a: ");
-        int a = scanner.nextInt();
-        System.out.println("nhap vao so b: ");
-        int b = scanner.nextInt();
-         
-       
-        int tong = a + b;
-        System.out.println(a + " + " + b + " = " + tong);
-         
-        int hieu = a - b;
-        System.out.println(a + " - " + b + " = " + hieu);
-         
-        int tich = a * b;
-        System.out.println(a + " * " + b + " = " + tich);
-         
-        float thuong = (float) a / b;
-        System.out.println(a + " / " + b + " = " + 
-                decimalFormat.format(thuong)); 
-         
-     
+        System.out.print("nhap he so bac 2, a = ");
+        float a =scanner.nextFloat();
+        System.out.print("nhap he so bac 1, b = ");
+        float b = scanner.nextFloat();
+        System.out.print("nhap he so tu do, c = ");
+        float c = scanner.nextFloat();
+        giaiPTBac2(a, b, c);
     }
- 
+     
+
+    public static void giaiPTBac2(float a, float b, float c) {
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("phuong trinh vo nghiem!");
+            } else {
+                System.out.println("Phuong trình có mot nghiem: "
+                        + "x = " + (-c / b));
+            }
+            return;
+        }
+        float delta = b*b - 4*a*c;
+        float x1;
+        float x2;
+        if (delta > 0) {
+            x1 = (float) ((-b + Math.sqrt(delta)) / (2*a));
+            x2 = (float) ((-b - Math.sqrt(delta)) / (2*a));
+            System.out.println("Phuong trinh có 2 nghiem la: "
+                    + "x1 = " + x1 + " và x2 = " + x2);
+        } else if (delta == 0) {
+            x1 = (-b / (2 * a));
+            System.out.println("Phuong trinh có nghiem kep: "
+                    + "x1 = x2 = " + x1);
+        } else {
+            System.out.println("Phuong trinh vo nghiem!");
+        }
+    }
 }
